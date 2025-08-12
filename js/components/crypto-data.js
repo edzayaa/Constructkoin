75e935c970f1c049ec7268369edc1da586b25a3b
Daniel Pacheco <137804389+StratoDev89@users.noreply.github.com>
2025-08-12 18:08:36 -0400

    quick fixes

--- js/components/crypto-data.js
+++ js/components/crypto-data.js
@@ -48,13 +48,17 @@ export class CryptoData {
     const tokenSoldElement = document.querySelector(".protocol-card__token-sold strong");
     const tokenSoldLineElement = document.querySelector(".protocol-card__loader-line");
     const currentPriceElement = document.querySelector(".protocol-card__current-price strong");
+    const heroCurrentPriceElement = document.querySelector(".hero-current-price");
+    const heroNextPriceElement = document.querySelector(".hero-next-price");
     const nextPriceElement = document.querySelector(".protocol-card__next-price strong");
 
     if (tokenSoldPercentageElement) tokenSoldPercentageElement.textContent = `${percentageSold}% Sold`;
     if (tokenSoldElement) tokenSoldElement.textContent = tokensSold.toLocaleString();
     if (tokenSoldLineElement) tokenSoldLineElement.style = `transform: translateX(${percentageSold * 100}%)`;
     if (currentPriceElement) currentPriceElement.textContent = `$${data.price}`;
+    if (heroCurrentPriceElement) heroCurrentPriceElement.textContent = `$${data.price}`;
     if (nextPriceElement) nextPriceElement.textContent = `$${nextPrice.toFixed(2)}`;
+    if (heroNextPriceElement) heroNextPriceElement.textContent = `$${nextPrice.toFixed(2)}`;
   }
 
   setupPriceButton() {
