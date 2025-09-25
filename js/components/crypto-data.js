e60e5c4ab5159b14ca8b501c59f5178acca4886e
ctkfrontendui <ctkfrontendui@gmail.com>
2025-09-25 12:47:49 +0300

    Update crypto-data.js

--- js/components/crypto-data.js
+++ js/components/crypto-data.js
@@ -13,22 +13,8 @@ export class CryptoData {
     const data = await this.cryptoService.getCryptoData();
     this.isFetching = false;
 
-    // const data = {
-    //   currentPhase: 1,
-    //   nextPhase: {
-    //     phaseNumber: 2,
-    //     tokenPrice: 0.109,
-    //   },
-    //   price: "0.1",
-    //   timeRemaining: 2484036,
-    //   tokensSold: "2102260.599114563",
-    //   totalTokens: "14894000.000000002",
-    //   total_tokens_sold: 2100054.0,
-    //   total_usdt_raised: 210005.40000000002,
-    // };
-
     if (data) {
-      this.currentPrice = data.price;
+      this.currentPrice = data.tokenPrice;
       this.nextPrice = data.nextPhase.tokenPrice;
       this.updateNavbarPrice(data.price);
       this.updateCardData(data);
@@ -46,11 +32,11 @@ export class CryptoData {
 
   updateCardData(data) {
     if (!data) return;
-
+    console.log(data)
     const tokensSold = parseFloat(data.tokensSold);
-    const totalTokens = parseFloat(data.totalTokens);
+    const totalTokens = parseFloat(data.tokenLimit);
     const percentageSold = ((tokensSold / totalTokens) * 100).toFixed(2);
-    const usdRaised = data.total_usdt_raised.toFixed(2);
+    const usdRaised = Number(data.total_usdt_raised).toFixed(2);
 
     const tokenSoldPercentageElement = document.querySelector(".protocol-card__loader-text");
     const tokenSoldElement = document.querySelector(".protocol-card__token-sold strong");
@@ -63,8 +49,8 @@ export class CryptoData {
     if (tokenSoldPercentageElement) tokenSoldPercentageElement.textContent = `${percentageSold}% Sold`;
     if (tokenSoldElement) tokenSoldElement.textContent = tokensSold.toLocaleString();
     if (tokenSoldLineElement) tokenSoldLineElement.style = `transform: translateX(${(tokensSold / totalTokens) * 100}%)`;
-    if (currentPriceElement) currentPriceElement.textContent = `$${data.price}`;
-    if (heroCurrentPriceElement) heroCurrentPriceElement.textContent = `$${data.price}`;
+    if (currentPriceElement) currentPriceElement.textContent = `$${data.tokenPrice}`;
+    if (heroCurrentPriceElement) heroCurrentPriceElement.textContent = `$${data.tokenPrice}`;
     if (usdRaisedElement) usdRaisedElement.textContent = `$${usdRaised}`;
 
     heroNextPriceElement.forEach((element) => {
